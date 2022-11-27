f1afb6ff0df823855e28b6e9ed88728406aa7fa1
Phil Urban <29461692+PhillyUrbs@users.noreply.github.com>
2022-11-27 13:58:06 -0500

    Add files via upload

---
+++ rebuild.ps1
@@ -0,0 +1,26 @@
+<#
+Leftovers
+    Yealink USB Connect
+    Razer Synapse
+    Battle.net Launcher
+#>
+
+$apps = {
+    "7zip.7zip",
+    "Corsair.iCUE.4",
+    "CreativeTechnology.CreativeApp",
+    "Discord.Discord",
+    "ElectronicArts.EADesktop",
+    "GOG.Galaxy",
+    "Microsoft.BingWallpaper",
+    "Microsoft.Office",
+    "Microsoft.VisualStudioCode",
+    "Nvidia.GeForceExperience",
+    "OpenWhisperSystems.Signal",
+    "Plex.Plex",
+    "Prusa3D.PrusaSlicer",
+    "PuTTY.PuTTY",
+    "SlackTechnologies.Slack",
+    "TechSmith.SnagIt.2023"
+    "Valve.Steam"
+}
